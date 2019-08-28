ba362878279aa14205e329d453bfc23405cfc4de
KagzNjenga <kagwimisamuel@gmail.com>
2019-08-28 16:25:25 -0400

    Initial Commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -72,6 +72,8 @@ public class Main extends Application {
         Scene scene = new Scene(grid, 300,275);
         primaryStage.setScene(scene);
 
+        scene.getStylesheets().add(Main.class.getResource("Login.css").toExternalForm());
+
         primaryStage.show();
     }
 
